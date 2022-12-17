924ce6dec55c17f0b2c550ce05caa01172147242
Sean van Zuidam <sean.grimlink@gmail.com>
2022-12-17 15:22:31 +0100

    ADD: copyright

--- ViewModel/StoreInfo.php
+++ ViewModel/StoreInfo.php
@@ -1,5 +1,11 @@
 <?php declare(strict_types=1);
 
+/**
+ * @author Siteation (https://siteation.dev/)
+ * @copyright Copyright 2021 Siteation (https://siteation.dev/)
+ * @license MIT
+ */
+
 namespace Siteation\StoreInfo\ViewModel;
 
 use Magento\Directory\Api\CountryInformationAcquirerInterface as CountryInfoAcq;
--- registration.php
+++ registration.php
@@ -1,5 +1,13 @@
 <?php declare(strict_types=1);
 
+/**
+ * StoreInfo module for Magento
+ *
+ * @author Siteation (https://siteation.dev/)
+ * @copyright Copyright 2021 Siteation (https://siteation.dev/)
+ * @license MIT
+ */
+
 use Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(
